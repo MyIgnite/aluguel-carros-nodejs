e9dd12c86df0b84623ca4c1ba908a897756daad5
Renan Moreira <renan.tsx@gmail.com>
2022-12-23 15:31:26 -0300

    inserindo uuid em categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,22 @@
 import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
-  const { name, descripion } = request.body;
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
 
-  categories.push({
+  const category = {
     name,
-    descripion
-  });
+    description,
+    id: uuidV4()
+  }
 
-  return response.status(201).send();
+  categories.push(category);
+
+  return response.status(201).json(category);
 });
 
 export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use("/categories", categoriesRoutes);
 
 app.listen(9090, function() {
   console.log('Server started on http://localhost:9090');
