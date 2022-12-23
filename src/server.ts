28f93826e1b71bcd9805f965eebdef8e59c05403
Renan Moreira <renan.tsx@gmail.com>
2022-12-23 14:23:19 -0300

    debugando a aplicação

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,18 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World"});
-})
+});
 
-app.listen(3333, function() {
-  console.log('Server started on http://localhost:3333/');
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
+
+app.listen(9090, function() {
+  console.log('Server started on http://localhost:9090/');
 });
