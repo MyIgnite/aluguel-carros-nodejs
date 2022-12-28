595806aaf6e4e348a5533ce145bda82e5e9f1b0c
Renan Moreira <renan.tsx@gmail.com>
2022-12-28 16:39:43 -0300

    Criando repositório de especificações

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,20 @@
+import { response, Router } from "express";
+
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationsRoutes = Router();
+
+const specificationRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", (request, esponse) => {
+  const { name, description } = request.body;
+
+  const createSpecificationService = new CreateSpecificationService(specificationRepository);
+
+  createSpecificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+export { specificationsRoutes };
