a1ff37043e754d8f371c357bb7f37fc12ed28e94
Renan Moreira <renan.tsx@gmail.com>
2023-01-12 17:51:15 -0300

    refatorando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from "express";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(9090, function() {
   console.log('Server started on http://localhost:9090');
