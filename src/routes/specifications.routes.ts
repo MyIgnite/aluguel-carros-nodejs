fbbc379bee33fc7fac745846733284612a9027f8
Renan Moreira <renan.tsx@gmail.com>
2023-01-12 14:03:53 -0300

    correção nomenclatura de UseCase

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { createSpecificationController } from "../modules/cars/useCases/createSpeficication";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 
