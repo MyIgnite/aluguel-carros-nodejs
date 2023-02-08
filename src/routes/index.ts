c50a2d8958d92bdd8a71e861188653ef35d0337d
Renan Moreira <renan.tsx@gmail.com>
2023-02-08 07:52:06 -0300

    Middlewares para tratamento de erros

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,15 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { authenticateRoutes } from "./authenticate.routes";
 
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
+
+function teste(request: Request, response: Response, next: NextFunction) {
+  console.log(request.method)
+  next()
+}
 
 const router = Router();
 
