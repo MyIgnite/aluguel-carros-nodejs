d971a7bb591f4667e43656f080f47a80fda19503
Renan Moreira <renan.tsx@gmail.com>
2023-02-20 00:51:49 -0300

    Funções para lidar com arquivos

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,15 +1,34 @@
+import { parse as csvParse } from "csv-parse";
 import fs from "fs";
 import { AppError } from "../errors/AppError";
 import HttpStatusCode from "../errors/HttpStatusCode";
 
-export const deleteFile = async (filename: string) => {
+type IResolveUploadCSV = Promise<Record<string, string>[]>
 
+const deleteFile = async (filename: string) => {
   try {
     await fs.promises.stat(filename);
-    
+    await fs.promises.unlink(filename);
   } catch (error) {
-    throw new AppError(error.message,HttpStatusCode.CONFLICT)
+    throw new AppError(error.message, HttpStatusCode.CONFLICT)
   }
-  
-  await fs.promises.unlink(filename);
 }
+
+const uploadCSV = async (file: Express.Multer.File): IResolveUploadCSV => {
+  return new Promise(async (resolve, reject) => { 
+
+    const rows: Record<string, string>[] = []
+    const stream = fs.createReadStream(file.path);
+    const parseFile = csvParse({ columns: true });
+    
+    return stream.pipe(parseFile)
+      .on("data", async (row) => rows.push(row))
+      .on("end", async () => await deleteFile(file.path).then(() => resolve(rows)))
+      .on("error", (err) => reject(err))
+  })
+}
+
+export {
+  deleteFile,
+  uploadCSV
+};
